e0f175435c7cbb34a5af518b60e752752658f1d9
zodiackiller1234567898 <83432873+zodiackiller1234567898@users.noreply.github.com>
2021-07-29 10:55:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach(  '#camera'  );
+
+
+function take_snapshot(){
+
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+}
+
+console.log('ml5 version',ml5.version);
+
+
+classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/byfR3a79l/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function check(){
+    img =document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    } 
+}
+
